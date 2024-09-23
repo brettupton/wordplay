af5bb555df93b34080c3fe0861e77e37b8307d02
brettupton <brettjupton@gmail.com>
2024-09-22 23:59:46 -0400

    fixed error with sql hydration

--- src/app/tags/brown/page.tsx
+++ src/app/tags/brown/page.tsx
@@ -1,41 +1,18 @@
-'use client'
-
-import { useEffect, useState } from "react"
+import { Suspense } from "react"
 import PageTable from "@/components/PageTable"
 
+export default async function Brown() {
+    const response = await fetch(`http://localhost:3000/api/tags?page=1&limit=100`, {
+        cache: 'no-store',
+    })
 
-export default function Brown() {
-    const [dbResults, setDBResults] = useState<any[]>([])
-    const [page, setPage] = useState<number>(1)
-    const [limit, setLimit] = useState<number>(100)
-    const [totalRows, setTotalRows] = useState<number>(0)
-
-    useEffect(() => {
-        const getBrownDB = async (page: number, limit: number) => {
-            try {
-                const response = await fetch(`/api/tags?page=${page}&limit=${limit}`, { method: 'GET' })
-                if (!response.ok) {
-                    throw new Error('Error fetching request')
-                }
-                const res = await response.json()
-
-                setDBResults([...res.dbResults])
-                setTotalRows(res.totalRows)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-
-        getBrownDB(page, limit)
-    }, [page, limit])
-
-    const setTablePage = (pageNum: number) => {
-        setPage(pageNum)
-    }
+    const { dbResults, totalRows } = await response.json()
 
     return (
-        <div className="flex">
-            <PageTable rows={dbResults} page={page} limit={limit} total={totalRows} setPage={setTablePage} />
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex">
+                <PageTable initial={dbResults} total={totalRows} />
+            </div>
+        </Suspense>
     )
 }
--- src/components/PageTable.tsx
+++ src/components/PageTable.tsx
@@ -1,13 +1,33 @@
+'use client'
+
+import { useState } from "react"
+
 interface PageTableProps {
-    rows: any[]
-    page: number
-    limit: number
+    initial: any[]
     total: number
-    setPage: (page: number) => void
 }
 
+export default function PageTable({ initial, total }: PageTableProps) {
+    const [dbResults, setDBResults] = useState<any[]>(initial)
+    const [page, setPage] = useState<number>(1)
+    const [limit, setLimit] = useState<number>(100)
 
-export default function PageTable({ rows, page, limit, total, setPage }: PageTableProps) {
+    const fetchPageData = async (pageNum: number) => {
+        if (pageNum > 0) {
+            try {
+                const response = await fetch(`/api/tags?page=${pageNum}&limit=${limit}`)
+                if (!response.ok) {
+                    throw new Error("Error fetching data.")
+                }
+                const data = await response.json()
+                setDBResults([...data.dbResults])
+                setPage(pageNum)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
+
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-400">
@@ -22,7 +42,7 @@ export default function PageTable({ rows, page, limit, total, setPage }: PageTab
                     </tr>
                 </thead>
                 <tbody>
-                    {rows.map((row, index) => {
+                    {dbResults.map((row, index) => {
                         return (
                             <tr className="bg-gray-800 border-b border-gray-700 hover:bg-gray-600" key={index}>
                                 <td className="px-6 py-4">
@@ -37,28 +57,28 @@ export default function PageTable({ rows, page, limit, total, setPage }: PageTab
                 </tbody>
             </table>
             <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
-                <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">{((page - 1) * limit) + 1}-{rows.length * page}</span> of <span className="font-semibold text-gray-900 dark:text-white">{total}</span></span>
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">{((page - 1) * limit) + 1}-{dbResults.length * page}</span> of <span className="font-semibold text-gray-900 dark:text-white">{total}</span></span>
                 <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
                     <li>
-                        <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                        <button onClick={() => fetchPageData(page - 1)} className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</button>
                     </li>
                     <li hidden={page - 2 <= 0}>
-                        <div onClick={() => setPage(page - 2)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page - 2}</div>
+                        <button onClick={() => fetchPageData(page - 2)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page - 2}</button>
                     </li>
                     <li hidden={page - 1 <= 0}>
-                        <div onClick={() => setPage(page - 1)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page - 1}</div>
+                        <button onClick={() => fetchPageData(page - 1)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page - 1}</button>
                     </li>
                     <li>
-                        <div onClick={() => setPage(page)} aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">{page}</div>
+                        <button onClick={() => fetchPageData(page)} aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">{page}</button>
                     </li>
                     <li>
-                        <div onClick={() => setPage(page + 1)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page + 1}</div>
+                        <button onClick={() => fetchPageData(page + 1)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page + 1}</button>
                     </li>
                     <li>
-                        <div onClick={() => setPage(page + 2)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page + 2}</div>
+                        <button onClick={() => fetchPageData(page + 2)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{page + 2}</button>
                     </li>
                     <li>
-                        <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                        <button onClick={() => fetchPageData(page + 1)} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</button>
                     </li>
                 </ul>
             </nav>
